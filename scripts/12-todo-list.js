b86edd974fd4720e22badf796ef877022c01a123
heartbreathing <li.wang@hyperisland.se>
2023-10-25 16:13:47 +0200

    new exercise

---
+++ scripts/12-todo-list.js
@@ -0,0 +1,54 @@
+const todos = [{
+  name: 'make dinner',
+  dueDate: '2023-10-16',
+}, {
+    name: 'wash dishes',
+  dueDate: '2023-10-16',
+}];
+
+renderTodoList();
+
+function renderTodoList () {
+  let todoListHtml = '';
+
+  for (let i = 0; i < todos.length; i++) {
+    const todoItem = todos[i];
+    const { name, dueDate } = todoItem;
+   
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+    todos.splice(${i}, 1);
+    renderTodoList();      
+    " class="delete-todo-button">Delete</button>
+    `;
+    todoListHtml += html;
+  }
+
+  document.querySelector('.js-todos').innerHTML = todoListHtml;
+}
+
+function addTodo () {  
+  const name = document.querySelector('.js-inputTodo').value;
+
+  const dueDate = document.querySelector('.js-due-date-input').value
+  // localStorage.setItem('todos', Number(todo));
+  todos.push({
+    name,
+    dueDate,
+  });
+  console.log(todos);
+  document.querySelector('.js-inputTodo').value = '';
+  document.querySelector('.js-due-date-input').value = '';
+  renderTodoList();
+}
+
+
+
+
+// let randomNumber = 0;
+// while (randomNumber < 0.5) {
+//   randomNumber = Math.random();
+// }
+// console.log(randomNumber)
