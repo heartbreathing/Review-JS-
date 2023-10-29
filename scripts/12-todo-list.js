69300a6f8d4ac04ffeaabfa6b2746451f1d2486e
heartbreathing <li.wang@hyperisland.se>
2023-10-29 23:34:11 +0100

    add some exercises

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -2,7 +2,7 @@ const todos = [{
   name: 'make dinner',
   dueDate: '2023-10-16',
 }, {
-    name: 'wash dishes',
+  name: 'wash dishes',
   dueDate: '2023-10-16',
 }];
 
@@ -11,28 +11,28 @@ renderTodoList();
 function renderTodoList () {
   let todoListHtml = '';
 
-  for (let i = 0; i < todos.length; i++) {
-    const todoItem = todos[i];
+  todos.forEach(function (todoItem, index) {
     const { name, dueDate } = todoItem;
-   
+
     const html = `
     <div>${name}</div>
     <div>${dueDate}</div>
     <button onclick="
-    todos.splice(${i}, 1);
+    todos.splice(${index}, 1);
     renderTodoList();      
     " class="delete-todo-button">Delete</button>
     `;
     todoListHtml += html;
-  }
+  });
+
 
   document.querySelector('.js-todos').innerHTML = todoListHtml;
 }
 
-function addTodo () {  
+function addTodo () {
   const name = document.querySelector('.js-inputTodo').value;
 
-  const dueDate = document.querySelector('.js-due-date-input').value
+  const dueDate = document.querySelector('.js-due-date-input').value;
   // localStorage.setItem('todos', Number(todo));
   todos.push({
     name,
